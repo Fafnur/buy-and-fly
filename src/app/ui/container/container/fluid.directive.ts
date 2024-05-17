9dce1128dcd98172d36d78df78fa0b5763449b82
Aleksandr Serenko <fafnur@yandex.ru>
2024-05-17 11:40:53 +0700

    [ui][container] Adding container component

---
+++ src/app/ui/container/container/fluid.directive.ts
@@ -0,0 +1,15 @@
+import { coerceBooleanProperty } from '@angular/cdk/coercion';
+import { Directive, HostBinding, Input } from '@angular/core';
+
+@Directive({
+  // eslint-disable-next-line @angular-eslint/directive-selector
+  selector: 'baf-container[fluid]',
+  standalone: true,
+})
+export class FluidDirective {
+  @Input() fluid: boolean | string | undefined | null;
+
+  @HostBinding('class.baf-fluid') get isFluid() {
+    return coerceBooleanProperty(this.fluid);
+  }
+}
---
+++ src/app/ui/container/container/mobile.directive.ts
@@ -0,0 +1,15 @@
+import { coerceBooleanProperty } from '@angular/cdk/coercion';
+import { Directive, HostBinding, Input } from '@angular/core';
+
+@Directive({
+  // eslint-disable-next-line @angular-eslint/directive-selector
+  selector: 'baf-container[mobile]',
+  standalone: true,
+})
+export class MobileDirective {
+  @Input() mobile: boolean | string | undefined | null;
+
+  @HostBinding('class.baf-mobile-no-gutter') get isMobile() {
+    return coerceBooleanProperty(this.mobile);
+  }
+}
