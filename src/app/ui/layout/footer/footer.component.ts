acd08675334c759a26e7a5d7c0a8654740e82cbb
Aleksandr Serenko <fafnur@yandex.ru>
2024-05-17 11:44:18 +0700

    [ui][layout] Adding footer styles

--- src/app/ui/layout/footer/footer.component.ts
+++ src/app/ui/layout/footer/footer.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
+import { ContainerComponent } from '@baf/ui/container';
+
 @Component({
   selector: 'baf-footer',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ContainerComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
