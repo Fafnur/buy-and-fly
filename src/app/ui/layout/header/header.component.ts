32fdc062765562bf82adf680838eb0e651904885
Aleksandr Serenko <fafnur@yandex.ru>
2024-05-19 09:57:28 +0700

    [ui][layout] Adding header styles

--- src/app/ui/layout/header/header.component.ts
+++ src/app/ui/layout/header/header.component.ts
@@ -1,13 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { ContainerComponent } from '@baf/ui/container';
+import { HomeComponent, LogoComponent } from '@baf/ui/icons';
 
 @Component({
   selector: 'baf-header',
   standalone: true,
-  imports: [],
+  imports: [LogoComponent, HomeComponent, ContainerComponent, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
