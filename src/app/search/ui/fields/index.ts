6b39662a11c3f618cd8e500193d72a7429ec2e82
Aleksandr Serenko <fafnur@yandex.ru>
2024-05-27 20:42:04 +0700

    [search] Moving fields

---
+++ src/app/search/ui/fields/index.ts
@@ -0,0 +1,5 @@
+export * from './libs/search-date/search-date.component';
+export * from './libs/search-destination/search-destination.component';
+export * from './libs/search-group/search-group.component';
+export * from './libs/search-passengers/search-passengers.component';
+export * from './libs/search-reverse/search-reverse.component';
--- src/app/search/ui/form/lib/search-form.component.ts
+++ src/app/search/ui/form/lib/search-form.component.ts
@@ -2,13 +2,14 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
 import { initialSearchForm } from '@baf/search/common';
 import { SearchService } from '@baf/search/services';
+import {
+  SearchDateComponent,
+  SearchDestinationComponent,
+  SearchGroupComponent,
+  SearchPassengersComponent,
+  SearchReverseComponent,
+} from '@baf/search/ui/fields';
 import { ButtonComponent } from '@baf/ui/buttons';
-
-import { SearchDateComponent } from './search-date/search-date.component';
-import { SearchDestinationComponent } from './search-destination/search-destination.component';
-import { SearchGroupComponent } from './search-group/search-group.component';
-import { SearchPassengersComponent } from './search-passengers/search-passengers.component';
-import { SearchReverseComponent } from './search-reverse/search-reverse.component';
 
 @Component({
   selector: 'baf-search-form',
