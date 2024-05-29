ae0eb2b80b60d36580e7142dbbabc8f7d60a5049
Aleksandr Serenko <fafnur@yandex.ru>
2024-05-29 08:13:59 +0700

    [app] Replace router-outlet

--- src/app/home/page/lib/home-page.component.ts
+++ src/app/home/page/lib/home-page.component.ts
@@ -4,11 +4,12 @@ import { ContainerComponent } from '@baf/ui/container';
 
 import { MustTakeComponent } from './must-take/must-take.component';
 import { PromoComponent } from './promo/promo.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'baf-home-page',
   standalone: true,
-  imports: [ContainerComponent, PromoComponent, MustTakeComponent],
+  imports: [ContainerComponent, PromoComponent, MustTakeComponent, RouterOutlet],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
