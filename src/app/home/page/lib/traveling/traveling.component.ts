712f93bf5161379ab2a988777a43011025e4d6ff
Aleksandr Serenko <fafnur@yandex.ru>
2024-06-03 19:12:21 +0700

    [home] Adding traveling block

--- src/app/home/page/lib/traveling/traveling.component.ts
+++ src/app/home/page/lib/traveling/traveling.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { ButtonComponent } from '@baf/ui/buttons';
 import { HeadlineComponent } from '@baf/ui/headline';
 import { TitleComponent } from '@baf/ui/title';
 
 @Component({
   selector: 'baf-traveling',
   standalone: true,
-  imports: [TitleComponent, HeadlineComponent],
+  imports: [TitleComponent, HeadlineComponent, ButtonComponent],
   templateUrl: './traveling.component.html',
   styleUrl: './traveling.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
