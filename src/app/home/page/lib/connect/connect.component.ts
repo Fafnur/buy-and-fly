e3011cf785277a92703133b9f03de9ce2344e203
Aleksandr Serenko <fafnur@yandex.ru>
2024-06-04 08:37:23 +0700

    [home] Adding connect

---
+++ src/app/home/page/lib/connect/connect.component.ts
@@ -0,0 +1,20 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { PathPipe, PATHS } from '@baf/core';
+import { AnchorComponent } from '@baf/ui/buttons';
+import { CardComponent } from '@baf/ui/cards';
+import { HeadlineComponent } from '@baf/ui/headline';
+import { TitleComponent } from '@baf/ui/title';
+
+@Component({
+  selector: 'baf-connect',
+  standalone: true,
+  imports: [TitleComponent, HeadlineComponent, CardComponent, AnchorComponent, RouterLink, PathPipe],
+  templateUrl: './connect.component.html',
+  styleUrl: './connect.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ConnectComponent {
+  readonly paths = PATHS;
+}
--- src/app/home/page/lib/home-page.component.ts
+++ src/app/home/page/lib/home-page.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 import { ContainerComponent } from '@baf/ui/container';
 
+import { ConnectComponent } from './connect/connect.component';
 import { ConvenientWithUsComponent } from './convenient-with-us/convenient-with-us.component';
 import { MustBuyComponent } from './must-buy/must-buy.component';
 import { PromoComponent } from './promo/promo.component';
@@ -11,7 +12,15 @@ import { TravelingComponent } from './traveling/traveling.component';
 @Component({
   selector: 'baf-home-page',
   standalone: true,
-  imports: [RouterOutlet, ContainerComponent, PromoComponent, MustBuyComponent, TravelingComponent, ConvenientWithUsComponent],
+  imports: [
+    RouterOutlet,
+    ContainerComponent,
+    PromoComponent,
+    MustBuyComponent,
+    TravelingComponent,
+    ConvenientWithUsComponent,
+    ConnectComponent,
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
