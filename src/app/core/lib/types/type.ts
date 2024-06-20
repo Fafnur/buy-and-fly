efb77c8cb1021dd8d27e6b8613c7348705ebfde0
Aleksandr Serenko <fafnur@yandex.ru>
2024-06-20 10:41:21 +0700

    [search] Fixing styles

--- src/app/core/lib/types/type.ts
+++ src/app/core/lib/types/type.ts
@@ -1,11 +1,12 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+/*  eslint-disable @typescript-eslint/no-explicit-any */
 export type ChangeFn = (value: any) => void;
+
 export type TouchedFn = () => void;
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+
 export type DisplayFn = (value: any, index?: number) => string;
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+
 export type MaskFn = (value: any) => string;
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+
 export type StyleFn = (value?: any) => string | string[];
 
 export type CoerceBoolean = boolean | string | undefined | null;
