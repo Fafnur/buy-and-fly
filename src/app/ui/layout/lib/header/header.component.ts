018e8527765dc3a8679afff077e3ad3c4504d2b2
Aleksandr Serenko <fafnur@yandex.ru>
2024-07-03 17:29:20 +0700

    [scss] Fixing imports

--- src/app/ui/layout/lib/header/header.component.ts
+++ src/app/ui/layout/lib/header/header.component.ts
@@ -2,9 +2,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { PathPipe, PATHS } from '@baf/core';
+import { AnchorComponent } from '@baf/ui/buttons';
 import { ContainerComponent } from '@baf/ui/container';
 import { HomeComponent, LogoComponent } from '@baf/ui/icons';
-import { AnchorComponent } from '@baf/ui/buttons';
 
 @Component({
   selector: 'baf-header',
